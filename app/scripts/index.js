26ca8dbe5df6056459ee694457bece2f6f33c43d
Devin Readel <readeldj@gmail.com>
2015-02-25 12:05:26 -0600

    working on FB push

---
+++ app/scripts/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var fbUrl = 'https://jdtictactoe.firebaseio.com/',
+       fb = new Firebase(fbUrl),
+       game = {
+       	            user1:"",
+       	            user2:"",
+       	            board:[]
+       };
+console.log('JS loaded');
+$('#newGame').on('click', function() {
+    fb.push(game);
+    console.log('clicked');
+	});
