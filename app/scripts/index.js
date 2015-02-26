5dc2172685296f1dee1046637ffcf5dcc9e82e22
Devin Readel <readeldj@gmail.com>
2015-02-26 14:08:26 -0600

    game poilished up prior to adding firebase, coloring removed, table styled in css

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,16 +5,12 @@ var fbUrl = 'https://jdtictactoe.firebaseio.com/games',
        game = {
        	            user1:"",
        	            user2:"",
-       	            board:[]
+       	            board:[['', '', ''], ['', '', ''], ['', '', '']],
+       	            isPlayer1Turn: true
        },
-     isPlayer1Turn = true,
-     gameList,
-     board = [['', '', ''], ['', '', ''], ['', '', '']],
-     state = board,
-     player = 'Player 1',
-     gameOver = false;
+     player = 'Player 1';
 
-drawBoard(board);
+drawBoard(game.board);
 
 function drawBoard(boardArray) {
   var $table = $('<table class="game_board table-bordered"></table>');
@@ -28,77 +24,89 @@ function drawBoard(boardArray) {
   $('.container').append($table);
 };
 
+function resetGame () {
+	game.board = [['', '', ''], ['', '', ''], ['', '', '']];
+	$('.game_board').remove();
+	drawBoard(game.board);
+};
 
-
-$('.game_board').on('click', 'td', function() {
+$('.container').on('click', 'td', function() {
   var cellCoord  = $(this).index(),
       rowCoord = $(this).closest('tr').index();
   $('.wrongMove').remove();
-  if($(this).hasClass('red_background') || $(this).hasClass('blue_background')) {
+  if($(this).text()=== "X" || $(this).text()==="O") {
     $('.container').append('<span class="wrongMove">Invalid move. That space has been claimed.</span>');
   } else {
-	  if(isPlayer1Turn) {
-	    $(this).addClass('red_background');
-	  } else {
-	    $(this).addClass('blue_background');
-	  }
-  if (isPlayer1Turn === true){
-     board[rowCoord][cellCoord] = 'X';
+  if (game.isPlayer1Turn === true){
+     game.board[rowCoord][cellCoord] = 'X';
    } else {
-     board[rowCoord][cellCoord] = 'O';
+     game.board[rowCoord][cellCoord] = 'O';
    }
-  toggleTurn(isPlayer1Turn);
-  checkWinner();
+  $('.game_board').remove();
+  drawBoard(game.board);
+  checkWinner(game.board);
+  toggleTurn(game.isPlayer1Turn);
   }
 });
 
-function checkWinner() {
+function checkWinner(state) {
 	//horizontal 1st row
   
   if (state[0][0] !== "" && state[0][0] === state[0][1] && state[0][0] === state[0][2]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //horizontal 2nd row
   else if (state[1][0] !== "" && state[1][0] === state[1][1] && state[1][0] === state[1][2]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //horizontal 3rd row
   else if (state[2][0] !== "" && state[2][0] === state[2][1] && state[2][0] === state[2][2]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //vertical 1st column
   else if (state[0][0] !== "" && state[0][0] === state[1][0] && state[1][0] === state[2][0]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //vertical 2nd column
   else if (state[0][1] !== "" && state[0][1] === state[1][1] && state[0][1] === state[2][1]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //vertical 3rd column
   else if (state[0][2] !== "" && state[0][2] === state[1][2] && state[0][2] === state[2][2]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //diagonal right
   else if (state[0][0] !== "" && state[0][0] === state[1][1] && state[0][0] === state[2][2]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //diagonal left
   else if (state[0][2] !== "" && state[0][2] === state[1][1] && state[0][2] === state[2][0]) {
     alert(player + ' is the winner!'),
-    gameOver = true;
+    resetGame();
   }
   //Draw
   else if (state[0][0] !== "" && state[0][1] !== "" && state[0][2] !== "" && state[1][0] !== "" && state[1][1] !== "" && state[1][2] !== "" && state[2][0] !== "" && state[2][1] !== "" && state[1][1] !== "") {
-  	alert('DRAW');
+  	alert('DRAW'),
+  	resetGame();
   	 }
 };
+
+function toggleTurn (userTurn) {
+  if (userTurn) {
+    game.isPlayer1Turn = false,
+    player = 'Player 2';
+  } else if (userTurn === false) {
+    game.isPlayer1Turn = true,
+    player = 'Player 1';;
+  }
+}
 
 //$('form').submit(function(evt){
 //	//User enters name.
@@ -129,12 +137,8 @@ function checkWinner() {
     // }
 //});
 
-function toggleTurn (userTurn) {
-  if (userTurn) {
-    isPlayer1Turn = false,
-    player = 'Player 2';
-  } else if (userTurn === false) {
-    isPlayer1Turn = true,
-    player = 'Player 1';;
-  }
-}
+	  // if(game.isPlayer1Turn) {
+	  //   $(this).addClass('red_background');
+	  // } else {
+	  //   $(this).addClass('blue_background');
+	  // }
