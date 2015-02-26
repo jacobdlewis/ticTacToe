a5601ab57b4257fb9df6d63c5931efc3330e75fc
Devin Readel <readeldj@gmail.com>
2015-02-25 21:07:42 -0600

    setup new board in js. color squares still work

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -8,9 +8,23 @@ var fbUrl = 'https://jdtictactoe.firebaseio.com/games',
        	            board:[]
        },
      isPlayer1Turn = true,
-     gameList;
+     gameList,
+     board = [['', '', ''], ['', '', ''], ['', '', '']];
+drawBoard(board);
+function drawBoard(boardArray) {
+  var $table = $('<table class="game_board table-bordered"></table>');
+  boardArray.forEach(function(row) {
+    var $row = $('<tr></tr>');
+    row.forEach(function(cell){
+      $row.append('<td>'+ cell +'</td>');
+    });
+    $table.append($row);
+  });
+  $('.container').append($table);
+};
 
 $('.game_board').on('click', 'td', function() {
+	console.log($(this).index());
   if(isPlayer1Turn) {
     $(this).addClass('red_background');
   } else {
@@ -19,22 +33,22 @@ $('.game_board').on('click', 'td', function() {
   toggleTurn(isPlayer1Turn);
 });
 
-$('form').submit(function(evt){
-	//User enters name.
-	event.preventDefault();
-	var enteredUserName;
-	enteredUserName = $('#userNameInput').val();
-	//ask FB for games
-    fb.once('value', function(snap){
-    	gameList = snap.val();
-    	console.log(gameList);
-    });
-    //if game list is empty, create game.
-    debugger;
-    if (gameList === null) {
-    	game.user1 = enteredUserName;
-    	fb.push(game);
-    } 
+//$('form').submit(function(evt){
+//	//User enters name.
+//	event.preventDefault();
+	// var enteredUserName;
+	// enteredUserName = $('#userNameInput').val();
+	// //ask FB for games
+ //    fb.once('value', function(snap){
+ //    	gameList = snap.val();
+ //    	console.log(gameList);
+ //    });
+ //    //if game list is empty, create game.
+ //    debugger;
+ //    if (gameList === null) {
+ //    	game.user1 = enteredUserName;
+ //    	fb.push(game);
+ //    } 
     //else {
     // //	_.forEach(gameList, function(g){
     // 		if (g.user1 === "") {
@@ -46,7 +60,7 @@ $('form').submit(function(evt){
     // 		};
     // 	});
     // }
-});
+//});
 
 function toggleTurn (userTurn) {
   if (userTurn) {
